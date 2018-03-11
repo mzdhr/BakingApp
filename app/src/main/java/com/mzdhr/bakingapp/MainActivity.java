fced63585a6d469abfb9a23ba93a89424c075fb9
MohammadL <mzdhr@users.noreply.github.com>
2018-03-11 05:30:35 +0300

    Impl and Tested ButterKnife Lib

--- app/src/main/java/com/mzdhr/bakingapp/MainActivity.java
+++ app/src/main/java/com/mzdhr/bakingapp/MainActivity.java
@@ -5,11 +5,21 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.BindString;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    // Test ButterKnife Lib
+    @BindView(R.id.testtextView) TextView mTitleTextView;
+    @BindString(R.string.app_name) String mAppName;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ButterKnife.bind(this);
+
+        // Test ButterKnife Lib
+        mTitleTextView.setText(mAppName);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
