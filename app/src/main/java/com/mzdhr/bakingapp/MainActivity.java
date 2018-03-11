99972432f6d0390e700dce870927ab8871ebff7a
MohammadL <mzdhr@users.noreply.github.com>
2018-03-11 08:01:00 +0300

    Impl Recyclerview and changes in other dependencies versions

--- app/src/main/java/com/mzdhr/bakingapp/MainActivity.java
+++ app/src/main/java/com/mzdhr/bakingapp/MainActivity.java
@@ -4,12 +4,12 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.TextView;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -35,8 +35,8 @@ import butterknife.ButterKnife;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     // Test ButterKnife Lib
-    @BindView(R.id.testtextView)
-    TextView mTitleTextView;
+    @BindView(R.id.recipeRecyclerView)
+    RecyclerView mRecipeRecyclerView;
     @BindString(R.string.app_name)
     String mAppName;
 
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         // Test ButterKnife Lib
-        mTitleTextView.setText(mAppName);
+        //mTitleTextView.setText(mAppName);
         // Test Volley Lib
         requestByVolley();
         // Test GSON Lib
@@ -75,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
         JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
             @Override
             public void onResponse(JSONArray response) {
-                mTitleTextView.setText("Response: " + response.toString());
+                //mTitleTextView.setText("Response: " + response.toString());
                 // notifyDataSetChanged(;
                 // progress.dismiss(;
 
