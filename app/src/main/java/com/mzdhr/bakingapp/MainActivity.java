8ce4d57ba270f0b65bc244885350a8760cdea05e
MohammadL <mzdhr@users.noreply.github.com>
2018-03-11 06:07:22 +0300

    Impl and Tested Volley Lib on API Done

--- app/src/main/java/com/mzdhr/bakingapp/MainActivity.java
+++ app/src/main/java/com/mzdhr/bakingapp/MainActivity.java
@@ -5,16 +5,26 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
 
 import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
     // Test ButterKnife Lib
     @BindView(R.id.testtextView) TextView mTitleTextView;
     @BindString(R.string.app_name) String mAppName;
@@ -31,7 +41,8 @@ public class MainActivity extends AppCompatActivity {
 
         // Test ButterKnife Lib
         mTitleTextView.setText(mAppName);
-
+        // Test Volley
+        requestByVolley();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -41,6 +52,32 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private void requestByVolley() {
+        String url = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
+
+        // Create RequestQueue object
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        // Create JsonArray Request Object
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                mTitleTextView.setText("Response: " + response.toString());
+                // notifyDataSetChanged(;
+                // progress.dismiss(;
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // TODO: Handle error
+                Log.d(TAG, "onErrorResponse: " + error.getMessage());
+            }
+        });
+
+        // Start that Request Object
+        requestQueue.add(jsonArrayRequest);
     }
 
     @Override
