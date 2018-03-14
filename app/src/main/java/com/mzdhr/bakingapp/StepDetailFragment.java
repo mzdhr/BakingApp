95bfa7f47d0742fad9ca2007f58a2d986eaa3a1c
MohammadL <mzdhr@users.noreply.github.com>
2018-03-14 04:40:19 +0300

    Impl and Tested ExoPlayer Lib on URI Video Done

--- app/src/main/java/com/mzdhr/bakingapp/StepDetailFragment.java
+++ app/src/main/java/com/mzdhr/bakingapp/StepDetailFragment.java
@@ -1,15 +1,37 @@
 package com.mzdhr.bakingapp;
 
 import android.app.Activity;
-import android.support.design.widget.CollapsingToolbarLayout;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 import com.mzdhr.bakingapp.dummy.DummyContent;
 
 /**
@@ -17,8 +39,20 @@ import com.mzdhr.bakingapp.dummy.DummyContent;
  * This fragment is either contained in a {@link StepsActivity}
  * in two-pane mode (on tablets) or a {@link StepDetailActivity}
  * on handsets.
+ *
+ *
+ *
+ * I use ExoPlayer Lib
+ * https://google.github.io/ExoPlayer/guide.html
  */
-public class StepDetailFragment extends Fragment {
+
+public class StepDetailFragment extends Fragment implements OnClickListener, ExoPlayer.EventListener{
+   // @BindView(R.id.player_view) PlayerView mPlayerView;
+    private SimpleExoPlayer mPlayer;
+
+    public String mVideoUrl = "";
+
+
     private static String TAG = StepDetailFragment.class.getSimpleName();
 
     /**
@@ -31,6 +65,8 @@ public class StepDetailFragment extends Fragment {
      * The dummy content this fragment is presenting.
      */
     private DummyContent.DummyItem mItem;
+    private PlayerView mPlayerView;
+    private MediaSessionCompat mMediaSession;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -43,6 +79,9 @@ public class StepDetailFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate: Launched!");
+        mVideoUrl = MainActivity.mRecipes.get(0).getSteps().get(0).getVideoURL();
+        Log.d(TAG, "onCreate: mVideoUrl: " + mVideoUrl);
+
 
         if (getArguments().containsKey(ARG_ITEM_ID)) {
             // Load the dummy content specified by the fragment
@@ -62,15 +101,164 @@ public class StepDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.item_detail, container, false);
 
-//        MainActivity.mRecipes.get(0).getIngredients().size();
+        // Preparing View Player
+        mPlayerView = rootView.findViewById(R.id.player_view);
+
+        // Preparing Media Session
+        mMediaSession = new MediaSessionCompat(getActivity(), TAG);
+        mMediaSession.setFlags(
+                MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS |
+                MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS);
+        mMediaSession.setMediaButtonReceiver(null);
+        // Preparing PlaybackState for media buttons
+        PlaybackStateCompat.Builder stateBuilder = new PlaybackStateCompat.Builder()
+                .setActions(
+                        PlaybackStateCompat.ACTION_PLAY |
+                                PlaybackStateCompat.ACTION_PAUSE |
+                                PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS |
+                                PlaybackStateCompat.ACTION_PLAY_PAUSE);
+
+        mMediaSession.setPlaybackState(stateBuilder.build());
+        mMediaSession.setCallback(new MySessionCallback());
+        mMediaSession.setActive(true);
+
+
+        // Preparing the Player
+        if (mPlayer == null) {
+            TrackSelector trackSelector = new DefaultTrackSelector();
+            LoadControl loadControl = new DefaultLoadControl();
+            mPlayer = ExoPlayerFactory.newSimpleInstance(getActivity(), trackSelector, loadControl);
+            mPlayerView.setPlayer(mPlayer);
+
+            mPlayer.addListener(this);
+
+            // Preparing the MediaSource
+            String userAgent = Util.getUserAgent(getActivity(), "BakingApp");
+            DefaultDataSourceFactory dataSourceFactory = new DefaultDataSourceFactory(getActivity(), userAgent);
+            DefaultExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+
+            MediaSource mediaSource = new ExtractorMediaSource(
+                    Uri.parse(mVideoUrl),
+                    dataSourceFactory,
+                    extractorsFactory,
+                    null,
+                    null);
+
+            mPlayer.prepare(mediaSource);
+            mPlayer.setPlayWhenReady(true);
+        }
+
+
+
         String value = MainActivity.mRecipes.get(0).getSteps().get(1).getDescription();
         ((TextView) rootView.findViewById(R.id.item_detail)).setText(value);
-
-//        // Show the dummy content as text in a TextView.
-//        if (mItem != null) {
-//            ((TextView) rootView.findViewById(R.id.item_detail)).setText(mItem.details);
-//        }
-
         return rootView;
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Resume Playing
+        mPlayer.setPlayWhenReady(true);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        // Pause Playing
+        mPlayer.setPlayWhenReady(false);
+    }
+
+    /**
+     * Release the player when the activity is destroyed.
+     */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        // Stop Playing
+        mPlayer.stop();
+        mPlayer.release();
+        mPlayer = null;
+        mMediaSession.setActive(false);
+    }
+
+
+    @Override
+    public void onTimelineChanged(Timeline timeline, Object manifest, int reason) {
+
+    }
+
+    @Override
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+    }
+
+    @Override
+    public void onLoadingChanged(boolean isLoading) {
+
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+
+    }
+
+    @Override
+    public void onRepeatModeChanged(int repeatMode) {
+
+    }
+
+    @Override
+    public void onShuffleModeEnabledChanged(boolean shuffleModeEnabled) {
+
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+
+    }
+
+    @Override
+    public void onPositionDiscontinuity(int reason) {
+
+    }
+
+    @Override
+    public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+
+    }
+
+    @Override
+    public void onSeekProcessed() {
+
+    }
+
+
+    /**
+     * Media Session Callbacks, where all external clients control the player.
+     */
+    private class MySessionCallback extends MediaSessionCompat.Callback {
+        @Override
+        public void onPlay() {
+            mPlayer.setPlayWhenReady(true);
+        }
+
+        @Override
+        public void onPause() {
+            mPlayer.setPlayWhenReady(false);
+        }
+
+        @Override
+        public void onSkipToPrevious() {
+            mPlayer.seekTo(0);
+        }
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
 }
