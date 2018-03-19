25a950dd99976895161242bf097b3012d528063b
MohammadL <mzdhr@users.noreply.github.com>
2018-03-19 10:57:35 +0300

    Test UI with Espresso Done

--- app/src/androidTest/java/com/mzdhr/bakingapp/MainActivityTest.java
+++ app/src/androidTest/java/com/mzdhr/bakingapp/MainActivityTest.java
@@ -1,6 +1,8 @@
 package com.mzdhr.bakingapp;
 
+import android.support.test.espresso.Espresso;
 import android.support.test.espresso.IdlingResource;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -12,37 +14,40 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
 /**
  * Created by mohammad on 17/03/2018.
  * This class test MainActivity.java UI
  * Test start after data retrieve from API, then it enter each item and check if its title correct.
- *
  */
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
 
-
     @Rule
-    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class, true, true);
+    public ActivityTestRule<MainActivity> mActivityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
 
-    private MainActivity mMainActivity = null;
     private IdlingResource mIdlingResource;
 
     @Before
     public void registerIdlingResource() {
-        mMainActivity = mActivityTestRule.getActivity();
-
+        mIdlingResource = mActivityTestRule.getActivity().getIdlingResource();
+        Espresso.registerIdlingResources(mIdlingResource);
     }
 
     @Test
-    public void titleTest01(){
-
+    public void retrieveDataFromAPIAndSelectFirstResult() {
+        onView(withId(R.id.recipeRecyclerView)).perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
     }
 
-
     @After
-    public void unregisterIdlingResource(){
-        // mMainActivity = null;
+    public void unregisterIdlingResource() {
+        if (mIdlingResource != null) {
+            Espresso.unregisterIdlingResources(mIdlingResource);
+        }
     }
 }
--- app/src/main/java/com/mzdhr/bakingapp/ui/activity/DataDownloader.java
+++ app/src/main/java/com/mzdhr/bakingapp/ui/activity/DataDownloader.java
@@ -1,17 +1,10 @@
 package com.mzdhr.bakingapp.ui.activity;
 
-import android.os.Bundle;
-import android.support.annotation.NonNull;
+
+import android.content.Context;
+import android.os.Handler;
 import android.support.annotation.Nullable;
-import android.support.annotation.VisibleForTesting;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -20,8 +13,6 @@ import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.mzdhr.bakingapp.R;
-import com.mzdhr.bakingapp.adapter.RecipeAdapter;
 import com.mzdhr.bakingapp.model.Ingredient;
 import com.mzdhr.bakingapp.model.Recipe;
 import com.mzdhr.bakingapp.model.Step;
@@ -31,101 +22,55 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+/**
+ * Takes a String and returns it after a while via a callback.
+ * <p>
+ * This executes a long-running operation on a different thread that results in problems with
+ * Espresso if an {@link IdlingResource} is not implemented and registered.
+ */
 
-import butterknife.BindString;
-import butterknife.BindView;
-import butterknife.ButterKnife;
+/**
+ * Created by mohammad on 19/03/2018.
+ */
 
-public class MainActivity extends AppCompatActivity implements DataDownloader.DelayerCallback {
-    // For Testing Espresso
-    @Nullable
-    public SimpleIdlingResource mIdlingResource;
+public class DataDownloader {
 
-    @VisibleForTesting
-    @NonNull
-    public SimpleIdlingResource getIdlingResource() {
-        if (mIdlingResource == null) {
-            mIdlingResource = new SimpleIdlingResource();
+    private static final String TAG = DataDownloader.class.getSimpleName();
+
+    private static final int DELAY_MILLIS = 3000;
+    final static ArrayList<Recipe> mRecipes = new ArrayList<>();
+
+    interface DelayerCallback {
+        void onDone(ArrayList<Recipe> recipes);
+    }
+
+    static void requestDataByVolley(Context context, final DelayerCallback callback,
+                                    @Nullable final SimpleIdlingResource idlingResource) {
+        /**
+         * The IdlingResource is null in production as set by the @Nullable annotation which means
+         * the value is allowed to be null.
+         *
+         * If the idle state is true, Espresso can perform the next action.
+         * If the idle state is false, Espresso will wait until it is true before
+         * performing the next action.
+         */
+        if (idlingResource != null) {
+            idlingResource.setIdleState(false);
         }
-        return mIdlingResource;
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        getIdlingResource();
-        DataDownloader.requestDataByVolley(this, MainActivity.this, mIdlingResource);
-    }
-
-    @Override
-    public void onDone(ArrayList<Recipe> recipes) {
-        mRecipes = recipes;
-        setAdapter();
-    }
 
 
-
-
-
-    // Object
-    private static final String TAG = MainActivity.class.getSimpleName();
-    public static ArrayList<Recipe> mRecipes = new ArrayList<>();
-    private RecipeAdapter mRecipeAdapter;
-
-    // Views
-    @BindView(R.id.recipeRecyclerView)
-    public RecyclerView mRecipeRecyclerView;
-    @BindString(R.string.app_name)
-    public String mAppName;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        ButterKnife.bind(this);
-
-        requestDataByVolley();
-        setRecyclerView();
-        setAdapter();
-    }
-
-    private void setRecyclerView() {
-        // Determined if Table or Phone by using is_tablet.xml (one for normal, one for screen above w900dp) in values directory.
-        if (getResources().getBoolean(R.bool.isTablet)) {
-            GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
-            mRecipeRecyclerView.setLayoutManager(gridLayoutManager);
-            Log.d(TAG, "onCreate: It is a Tablet");
-        } else {
-            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-            mRecipeRecyclerView.setLayoutManager(linearLayoutManager);
-            Log.d(TAG, "onCreate: It is a Phone");
-        }
-        mRecipeRecyclerView.setHasFixedSize(true);
-    }
-
-    private void setAdapter() {
-        mRecipeAdapter = new RecipeAdapter(mRecipes, this);
-        mRecipeRecyclerView.setAdapter(mRecipeAdapter);
-    }
-
-    public void requestDataByVolley() {
         String url = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
 
         // Create RequestQueue object
-        final RequestQueue requestQueue = Volley.newRequestQueue(this);
+        final RequestQueue requestQueue = Volley.newRequestQueue(context);
 
         // Create JsonArray Request Object
         JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
             @Override
             public void onResponse(JSONArray response) {
-                //mTitleTextView.setText("Response: " + response.toString());
-                // notifyDataSetChanged(;
-                // progress.dismiss(;
 
                 JSONObject jsonObject = null;
-                mRecipes = new ArrayList<>();
+                //mRecipes = new ArrayList<>();
 
                 try {
                     for (int i = 0; i < response.length(); i++) {
@@ -173,7 +118,18 @@ public class MainActivity extends AppCompatActivity implements DataDownloader.De
 
                 }
 
-                setAdapter();
+
+
+                /**
+                 * {@link postDelayed} allows the {@link Runnable} to be run after the specified amount of
+                 * time set in DELAY_MILLIS elapses. An object that implements the Runnable interface
+                 * creates a thread. When this thread starts, the object's run method is called.
+                 *
+                 * After the time elapses, if there is a callback we return the image resource ID and
+                 * set the idle state to true.
+                 */
+
+
             }
         }, new Response.ErrorListener() {
             @Override
@@ -182,27 +138,23 @@ public class MainActivity extends AppCompatActivity implements DataDownloader.De
             }
         });
 
+
         // Start that Request Object
         requestQueue.add(jsonArrayRequest);
+        //  requestQueue.start();
+
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (callback != null) {
+                    callback.onDone(mRecipes);
+                    if (idlingResource != null) {
+                        idlingResource.setIdleState(true);
+                    }
+                }
+            }
+        }, DELAY_MILLIS);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-
-
-
-
 }
